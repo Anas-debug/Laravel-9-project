f996ea4687ca7ec46e478c3dad7f707db850953b
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-30 00:58:10 +0100

    Added about route

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -31,7 +31,6 @@
             <div class="container">
                 <div class="text-center my-5">
                     <h1 class="fw-bolder">About Us</h1>
-                    <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
                 </div>
             </div>
         </header>
@@ -39,7 +38,8 @@
         <div class="container">
             <div class="row">
                 <!-- Blog entries-->
-                <div class="col-lg-8">
+                <div class="col-lg-12">
+                <p class="lead mb-0">About us text</p>
                     <!-- Featured blog post-->
                     <div class="card mb-4">
                         <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
