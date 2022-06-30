e26efe14598cc1c0b551c2abcb51d495852cd4e8
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-30 17:52:29 +0100

    Made links dynamic by configuring routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('home');
 
-Route::view('/about', 'about');
+Route::view('/about', 'about')->name('about');
 
-Route::view('/contact', 'contact');
+Route::view('/contact', 'contact')->name('contact');
