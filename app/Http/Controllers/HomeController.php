43a60e4a89dbd5ed854e551a75211e1900910188
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-02 21:35:22 +0100

    Added one category for testing purposes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function index(){
         //Hardcoded data
-        $allCategories = ['Category 1', 'Category 2'];
+        $allCategories = ['Category 1', 'Category 2', 'Category 3'];
 
         return view('index', ['categories' => $allCategories]);
     }
